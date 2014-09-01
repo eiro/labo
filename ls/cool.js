3f9e1cdef04ba4f1c50f82fede6ef0ad79158666
Marc Chantreux <khatar@phear.org>
2014-09-01 17:49:36 +0200

    cool.js now running

--- ls/cool.js
+++ ls/cool.js
@@ -1,5 +1,7 @@
-loadPending = () ->
-    r = new XMLHttpRequest!
+loadPending = ->
+    r = new XMHttpRequest!
     r.onload -> (getElementById \slot).innerHTML = this.responseText
     r.open \get \http://example.com true
     r.send!
+    r 
+
